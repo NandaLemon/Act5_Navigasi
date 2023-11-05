24360f064fe4c129a5df93dd6e0db44f0a01bb50
NandaLemon <rizkynanda010703@gmail.com>
2023-11-05 22:38:23 +0700

    final commit

---
+++ app/src/main/java/com/example/navigasi/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.navigasi.data
+
+import com.example.navigasi.R
+
+object SumberData {
+    val flavor = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
